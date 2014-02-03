2acdabefa846632c7e42ddc4b23230a0984e448b
Peter <project_bravo@hotmail.com>
2014-02-04 00:15:56 +0100

    Started config file loading method

--- Doge-Market-Analyzer-Server/src/com/BitJaeger/Bot.java
+++ Doge-Market-Analyzer-Server/src/com/BitJaeger/Bot.java
@@ -5,6 +5,12 @@ package com.BitJaeger;
  * @author Peter
  *
  */
-public class Bot {
+public class Bot implements Runnable {
+
+	@Override
+	public void run() {
+		// Open the data file if it exists and prepare to write data
+		
+	}
 
 }
--- Doge-Market-Analyzer-Server/src/com/BitJaeger/main.java
+++ Doge-Market-Analyzer-Server/src/com/BitJaeger/main.java
@@ -1,14 +1,49 @@
 package com.BitJaeger;
 
+import java.io.File;
+import java.util.ArrayList;
+
+import org.json.simple.JSONObject;
+
 public class main {
 
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		System.out.println("DMA up");
+		
+		//Load the config
+		ArrayList<Bot> bots = loadConfig();
+		if(bots == null)
+			return;
+		
+		//Start the bots 
+		for(int i=0;i<bots.size();i++){
+			
+		}
+			
 	}
 	
-	private static void loadConfig() {
+	private static ArrayList<Bot> loadConfig() {
 		
+		System.out.println("Loading config file...");
+		
+		ArrayList<Bot> bots	= new ArrayList<Bot>();
+		File configFile		= new File("DMA-config.txt");
+		
+		//Check if the config file exists
+		if(!configFile.exists()){
+			System.out.println("No config file found, exiting...");
+			return null;
+		}
+		
+		//Load raw config file
+		StringBuilder input = new StringBuilder();
+		
+		//Convert to JSON
+		JSONObject configObject = new JSONObject();
+		
+		return bots;
 	}
 
 }
